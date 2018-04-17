0580738d26f78f283e54c89acfc547c5893edc60
Mathew Schlichting <matthew.schliching@gmail.com>
2018-04-17 17:29:34 -0500

    templating more

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ var path    = require(  'path'  );
 // external dependencies
 var multiparty  = require(  'multiparty');
 var express     = require(  'express'   );
-var sqlite3     = require('sqlite3').verbose();
 
 // local dependencies
 var mime = require('./mime');
@@ -28,26 +27,17 @@ app.use('/css', express.static(path.join(public_dir, 'css')));
 // home page should be static
 
 app.get('/', (req, res) => {
-   // database.init('./../imdb.sqlite3');
-    //database.select();
-
-
     res.sendFile(path.join(public_dir, 'index.html'));
 });
 
 
 
-
-
-
-
-
 // search
 app.post('/search', (req, res) =>{
-    var html;
     var form;
     var i;
     var results;
+    var page;
 
     form = new multiparty.Form();
     form.parse(req, (err, fields, files) => {
@@ -59,8 +49,8 @@ app.post('/search', (req, res) =>{
         else{
 
             // do search here
-            
-			// fields.type[0]       =   table name
+
+            // fields.type[0]       =   table name
             // fields.search[0]     =   search value
 			var id = '';
 			if(fields.type[0]=='Names'){
@@ -75,35 +65,40 @@ app.post('/search', (req, res) =>{
 
             results = [{id:'12345', name:'Brad Pitt the actor'}, {id:'12346', name:'Brad Pitt the non-actor'}];
 
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            html = '';
-            
-            html += '<!DOCTYPE html>';
-            html += '<html>';
-            html +=     '<head>';
-            html +=         '<title>Search Results</title>';
-            html +=     '</head>';
-            html +=     '<body>';
-            html +=         '<h1>Search Results for: ' + fields.search[0] + '</h1>';
-            html +=         '<div>We Don\'t have a database connection yet but here is test values</div>';
+            var template_location = path.join(public_dir, 'html');
 
-            html +=         '<ul>';
-            for(i=0; i<results.length; i++){
-                html +=         '<li>';
-                html +=             '<a href="http://localhost:8018/person/' + results[i].id + '">';
-                html +=                 results[i].name;
-                html +=             '</a>';
-                html +=         '</li>';
-            }
-            html +=         '</ul>';
+            fs.readFile(path.join(template_location, 'main_template.html'), (err, main_template) => {
+                if(err){
+                    res.writeHead(404, {'Content-Type': 'text/plain'});
+                    res.write('Couldn\'t find file');
+                    res.end();
+                } else{
+                    fs.readFile(path.join(template_location, 'results_template.html'), (err, result_template) => {
+                        if (err) {
+                            res.writeHead(404, {'Content-Type': 'text/plain'});
+                            res.write('Couldn\'t find file');
+                            res.end();
+                        }
+                        else {
+                            res.writeHead(200, {'Content-Type': 'text/html'});
+                            page = '' + main_template;
 
+                            page.replace('{{PAGE-TITLE}}', 'Results');
+                            page.replace('{{BODY}}', result_template + '');
+                            page.replace('{{SEARCH}}', fields.search[0]);
+                            if(fields.type[0] === 'a') {
+                                //swap results with actor results
+                            }
+                            else if(fields.type[0] === 'm'){
+                                //swap results with movie results
+                            }
 
-            html +=     '</body>';
-            html += '</html>';
-
-
-            res.write(html);
-            res.end();
+                            res.write(page);
+                            res.end();
+                        }
+                    });
+                }
+            });
         }
     });
 });
